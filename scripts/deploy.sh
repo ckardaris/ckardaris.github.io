84ea68758bcbb2ca76ef82c8861ff454b74b7330
Charalampos Kardaris <ckardaris@outlook.com>
2024-05-04 15:10:15 +0200

    Reduce font size to 18px

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ echo Pushing sources...
 git push origin sources
 cd _site
 [[ ! -d .git ]] && echo No git directory found... && exit 1
-git remote add origin git@github.com:ckardaris/ckardaris.github.io.git &/dev/null
+git remote add origin git@github.com:ckardaris/ckardaris.github.io.git &>/dev/null
 echo Pushing static site...
 ! git log -n 1 &>/dev/null && echo No commit found... && exit 1
 git push origin main
