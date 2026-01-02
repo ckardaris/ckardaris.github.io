a383baaeed7743c966b1b23a57704153fb9b43d5
Charalampos Kardaris <ckardaris@outlook.com>
2026-01-02 13:46:27 +0100

    [Photography] Better responsive images size

--- scripts/make-thumbnails.sh
+++ scripts/make-thumbnails.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-mkdir -p photography/thumbnails/{400x400,800x800}
 cd photography
 
-do_convert() {
-    for file in *
-    do
-        [[ -d "$file" ]] && continue
-        line=".............................................."
-        printf "%s %s" "[$1] $file" ${line:${#file}}
-        [[ -f thumbnails/$1/$file ]] && echo Skipping && continue
-        convert "$file" -resize $1 thumbnails/$1/$file
-        echo OK
-    done
+function do_convert {
+    line=".............................................."
+    file="$2"
+    printf "%s %s" "[$1] $file" ${line:${#file}}
+    [[ -f thumbnails/$1/$file ]] && echo Skipping && return
+    magick "$file" -resize $1 thumbnails/$1/$file
+    echo OK
+}
+export -f do_convert
+
+convert_all() {
+    mkdir -p thumbnails/"$1"
+    fd --type f --max-depth 1 -x sh -c "do_convert $1 {}"
 }
 
-do_convert 400x400
-do_convert 800x800
+convert_all 400x400
+convert_all 800x800
+convert_all 1200x1200
+convert_all 1600x1600
+convert_all 2400x2400
