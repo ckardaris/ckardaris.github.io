c140eadb75f7ebcb359dd675d5bf820ce857e77f
Charalampos Kardaris <ckardaris@outlook.com>
2026-01-22 19:13:32 +0100

    Hash password to before filling email body

--- _includes/send-email.js
+++ _includes/send-email.js
@@ -1,11 +1,19 @@
-function sendEmail(replyId = "") {
+async function sha256(str) {
+    const data = new TextEncoder().encode(str);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    return [...new Uint8Array(hashBuffer)]
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
+
+async function sendEmail(replyId = "") {
     const form = document.getElementById(`comment-form-${replyId}`);
     if (!form.checkValidity()) {
         return;
     }
     const username = document.getElementById(`username-${replyId}`).value.trim();
     const comment = document.getElementById(`comment-input-${replyId}`).value.trim();
-    const password = document.getElementById(`password-${replyId}`).value.trim();
+    const password = await sha256(document.getElementById(`password-${replyId}`).value.trim());
 
     const subject = "Comment: {{ page.title }}";
     let body = `post: {{ page.id }}`;
