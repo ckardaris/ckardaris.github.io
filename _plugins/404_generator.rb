8989be5f17fca170d3511c67a863ec6837e57cc2
Charalampos Kardaris <ckardaris@outlook.com>
2026-08-15 09:27:47 +0300

    Prepare for flatpak release
    - 404 all flatpak subdirectories
    - Add icon to flatpakrepo file

--- _plugins/404_generator.rb
+++ _plugins/404_generator.rb
@@ -2,21 +2,22 @@ require "find"
 require "pathname"
 
 module Jekyll
+
   class Assets404Generator < Generator
     safe true
     priority :normal
+    Targets = ["assets", "flatpak"]
 
     def generate(site)
       app_path = site.source
-
-      base_path = File.join(app_path, "assets")
-      return unless Dir.exist?(base_path)
-
-      Find.find(base_path) do |path|
-        next unless File.directory?(path)
-
-        relative_path = Pathname.new(path).relative_path_from(app_path).to_s()
-        site.pages << GeneratedPage.new(site, relative_path)
+      Targets.each do |target|
+        base_path = File.join(app_path, target)
+        next unless Dir.exist?(base_path)
+        Find.find(base_path) do |path|
+          next unless File.directory?(path)
+          relative_path = Pathname.new(path).relative_path_from(app_path).to_s()
+          site.pages << GeneratedPage.new(site, relative_path)
+        end
       end
     end
   end
@@ -29,8 +30,7 @@ module Jekyll
       @basename = 'index'
       @ext      = '.html'
       @name = "index.html"
-
-      Jekyll.logger.info "\r[assets_404.rb] Generating: #{@dir}/index.html"
+      Jekyll.logger.info "\r[404_generator.rb] Generating: #{@dir}/index.html"
       self.process(@name)
       self.data = {
         "layout" => "404",
